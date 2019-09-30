eaf95d5d9a5fa275349fcca053e58dd86404bb35
Danielle Burrage <burragedanielle@gmail.com>
2019-09-29 22:34:57 -0500

    added images and functionality with reset button to move from round to round

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,68 +23,87 @@
 
 // 1. User clicks start to begin
 
-$()
-
 // 2. game starts with RANDOM NUMBER displayed
-var randomNumber = Math.floor(Math.random() * 50);
-$("#random-number").html(randomNumber);
-    
+
+
     // game starts with zero user wins
     var wins = 0; 
     $("#wins").html(wins);
 
     // game starts with zero user losses 
+
     var losses = 0; 
-    $("losses").html(losses);
+    $("#losses").html(losses);
 
     //game starts with zero on the counter
+
     var totalScore = 0;
+    $("#user-total-score").html(totalScore);
+    
+    var crystalValues = [3, 6, 4, 7];
 
-// 3.  when user clicks CRYSTAL BUTTON
+    var crystalImages = ["./assets/images/amethyst.png","./assets/images/peridot.png", "./assets/images/sapphire.png", "./assets/images/quartz.png"];
 
-var crystalValues = [3, 6, 4, 7];
+var randomNumber = Math.floor(Math.random() * 50);
 
-var crystalImages = ["./assets/images/carole-smile-SSoCY-Xi3iY-unsplash.jpg", "./assets/images/carole-smile-SSoCY-Xi3iY-unsplash.jpg", "./assets/images/carole-smile-SSoCY-Xi3iY-unsplash.jpg"];
+function reset(){
+    totalScore = 0;
+};
 
-//***dev note: add an attribute for each button that holds the increments 
 
 for (var i = 0; i < crystalValues.length; i++) {
     var crystalDisplay = $("<img>");
     crystalDisplay.attr("crystal-data-value", crystalValues[i]);
     crystalDisplay.addClass("crystal-image");
 
-    for (var j = 0; j < crystalImages.length; j++) {
+    for (var j = 1; j < crystalImages.length; j++) {
         
         crystalDisplay.attr("src", crystalImages[j]);
     };
-    
     
     $(".crystals").append(crystalDisplay);
        
 };
 
-    $(".crystal-image").on("click", function() {
 
+$("#start-button").on("click", function() {
+    randomNumber = Math.floor(Math.random() * 50);
+    $("#random-number").html(randomNumber);
+    $("#start-button").html("Restart Game");
+
+});
+
+// 3.  when user clicks CRYSTAL BUTTON
+
+//***dev note: add an attribute for each button that holds the increments 
+
+    $(".crystal-image").on("click", function() {
         var crystalValue = ($(this).attr("crystal-data-value"));
-        crystalValue = parseInt(crystalValue);
-        totalScore+= crystalValue;
-        
-        alert("New score " + totalScore + " !")
+        totalScore += parseInt(crystalValue);
+        $("#user-total-score").html(totalScore);
 
     // 4. user wins if
         //- TOTAL SCORE = RANDOM NUMBER
         if(totalScore === randomNumber) {
             alert("You win!");
-            wins++; 
+            wins+= 1
+            console.log(wins);
+            $("#wins").html(wins);
+            reset();
 
     // 5. user loses if
         // - TOTAL SCORE > RANDOM NUMBER 
         } else if (totalScore > randomNumber) {
             alert("You lose!");
-            losses++; 
-        }
-        
+            losses+= 1
+            console.log(losses);
+            $("#losses").html(losses);
+            reset();
+            $("#user-total-score").html(totalScore);
+        };  
     });
+
+
 
 // 6. when user wins or loses, game restarts
         // - new RANDOM NUMBER
